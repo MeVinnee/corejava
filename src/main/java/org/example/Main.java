5b8677c3859c2d42880935a7371da181a5f0c4b3
Vinnee <vanita.patil@outlook.com>
2024-09-26 00:41:13 +0530

    Added Locators

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,37 @@
 package org.example;
 
 //import com.sun.org.apache.bcel.internal.generic.SWAP;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/vanit/OneDrive/Documents/SeleniumFiles/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/locatorspractice");
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.name("inputPassword")).sendKeys("rahul");
+        driver.findElement(By.className("submit")).click();
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Vanita");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("vanita.xyz@gmail.com");
+        driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("vanita.patil@gmail.com");
+        driver.findElement(By.xpath("//form/input[3]")).sendKeys("00000000");
+        driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
+        System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+        driver.quit();
+
+
 
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -51,8 +77,8 @@ public class Main {
 
 //            Arrays pr2 = new Arrays();
 //            pr2.Practice2();
-
-            Arrays pr3 = new Arrays();
-            pr3.Practice3();
+//
+//            Arrays pr3 = new Arrays();
+//            pr3.Practice3();
     }
 }

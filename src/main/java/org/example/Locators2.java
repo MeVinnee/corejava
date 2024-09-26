c0dec34d0db3e64908017744c158cf8d840c5502
Vinnee <vanita.patil@outlook.com>
2024-09-26 19:38:57 +0530

    Added Dynamic Password

--- src/main/java/org/example/Locators2.java
+++ src/main/java/org/example/Locators2.java
@@ -15,9 +15,11 @@ public class Locators2 {
         System.setProperty("webdriver.chrome.driver", "C:/Users/vanit/OneDrive/Documents/SeleniumFiles/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+        String password = getPassword(driver);
+
         driver.get("https://rahulshettyacademy.com/locatorspractice");
         driver.findElement(By.id("inputUsername")).sendKeys(name);
-        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.name("inputPassword")).sendKeys(password);
         driver.findElement(By.className("submit")).click();
         Thread.sleep(1000);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -27,5 +29,23 @@ public class Locators2 {
         driver.findElement(By.xpath("//button[text()='Log Out']")).click();
         driver.close();
 
+    }
+    public static String getPassword(WebDriver driver) throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/locatorspractice");
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
+
+        String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+        //Please use temporary password 'rahulshettyacademy' to Login.
+        String[] passwordArray1 = passwordText.split("'");
+        //Please use temporary password [0]
+        //rahulshettyacademy' to Login. [1]
+//        String [] passwordArray2 = passwordArray1 [1].split("'");
+//        String password = passwordArray2 [0]
+          String password;
+          password = passwordArray1 [1].split("'") [0];
+
+        return password;
     }
 }

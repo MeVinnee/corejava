0114dac5e58cf06f1c502a80298c4fc9acbb8054
Vinnee <vanita.patil@outlook.com>
2024-09-28 00:40:33 +0530

    Created new class of UpdateDropdown and called it from main method

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # This is selenium-core-java project.
 ## Auther - Vinnee
+## Official Website for ChromeDriver - chromedriver.chromium.org/downloads
+(https://googlechromelabs.github.io/chrome-for-testing/)
+
+
--- src/main/java/org/example/Dropdowns.java
+++ src/main/java/org/example/Dropdowns.java
@@ -6,12 +6,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Dropdowns {
-    public static void main (String [] args){
-        System.setProperty("webdriver.chrome.driver", "C:/Users/vanit/OneDrive/Documents/SeleniumFiles/chromedriver.exe");
+    public static void main (String [] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/vanit/OneDrive/Documents/SeleniumFiles/New folder/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-        Dropdowns obj = new Dropdowns();
-        obj.staticDropdown(driver);
+
+        //StaticDropdown
+//        Dropdowns obj = new Dropdowns();
+//        obj.staticDropdown(driver);
+        Dropdowns obj1 = new Dropdowns();
+        obj1.updateDropdown(driver);
+//      ChormeVersion is Version 129.0.6668.71
     }
     public void staticDropdown(WebDriver driver){
 
@@ -23,6 +28,18 @@ public class Dropdowns {
         System.out.println(dropdown.getFirstSelectedOption().getText());
         dropdown.selectByValue("INR");
         System.out.println(dropdown.getFirstSelectedOption().getText());
+        driver.close();
+    } public void updateDropdown(WebDriver driver) throws InterruptedException {
+
+        driver.findElement(By.id("divpaxinfo")).click();
+        Thread.sleep(2000);
+        int i = 1;
+        while (i<5){
+            driver.findElement(By.id("hrefIncAdt")).click();
+            i++;
+        }
+        driver.findElement(By.id("hrefIncChd")).click();
+        driver.findElement(By.id("btnclosepaxoption"));
         driver.close();
     }
 }

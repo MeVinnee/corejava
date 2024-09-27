353dc4b432d7ae40041b3f92ee432e425c629fb5
Vinnee <vanita.patil@outlook.com>
2024-09-28 00:46:45 +0530

    Added For Loop for 'UpdateDropdown'

--- src/main/java/org/example/Dropdowns.java
+++ src/main/java/org/example/Dropdowns.java
@@ -31,15 +31,24 @@ public class Dropdowns {
         driver.close();
     } public void updateDropdown(WebDriver driver) throws InterruptedException {
 
-        driver.findElement(By.id("divpaxinfo")).click();
-        Thread.sleep(2000);
-        int i = 1;
-        while (i<5){
-            driver.findElement(By.id("hrefIncAdt")).click();
-            i++;
-        }
-        driver.findElement(By.id("hrefIncChd")).click();
-        driver.findElement(By.id("btnclosepaxoption"));
-        driver.close();
+//        driver.findElement(By.id("divpaxinfo")).click();
+//        Thread.sleep(2000);
+//        int i = 1;
+//        while (i<5){
+//            driver.findElement(By.id("hrefIncAdt")).click();
+//            i++;
+//        }
+//        driver.findElement(By.id("hrefIncChd")).click();
+//        driver.findElement(By.id("btnclosepaxoption"));
+//        driver.close();
+
+          driver.findElement(By.id("divpaxinfo")).click();
+          Thread.sleep(2000);
+          for (int i=1; i<5; i++){
+              driver.findElement(By.id("hrefIncAdt")).click();
+          }
+          driver.findElement(By.id("hrefIncChd")).click();
+          driver.findElement(By.id("btnclosepaxoption"));
+          driver.close();
     }
 }

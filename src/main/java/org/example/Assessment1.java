8a602fc71b357c2b51af4ed2701653b5b44db649
Vinnee <vanita.patil@outlook.com>
2024-10-01 11:41:53 +0530

    Count of Checkboxes

--- src/main/java/org/example/Assessment1.java
+++ src/main/java/org/example/Assessment1.java
@@ -3,20 +3,24 @@ package org.example;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
 
 public class Assessment1 {
     public static void main (String[] args){
         System.setProperty("webdriver.chrome.driver", "C:/Users/vanit/OneDrive/Documents/SeleniumFiles/New folder/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+//
+        Assessment1 obj1 =new Assessment1();
+        obj1.practice1(driver);
+
+    }
+    public void practice1 (WebDriver driver){
+        System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+
         driver.findElement(By.id("checkBoxOption1")).click();
         Assert.assertTrue(driver.findElement(By.id("checkBoxOption1")).isSelected());
         System.out.println(driver.findElement(By.id("checkBoxOption1")).isSelected());
-
+        driver.quit();
     }
-
 }
